76ce619992230c142eb5d30e8665ed01ea17fba0
djtullox <djtullox@gmail.com>
2026-05-24 12:34:10 -0500

    Update ScanScreen.jsx

--- src/screens/ScanScreen.jsx
+++ src/screens/ScanScreen.jsx
@@ -214,7 +214,6 @@ export default function ScanScreen() {
             ref={fileInputRef}
             type="file"
             accept="image/*"
-            capture="environment"
             multiple
             onChange={handleFileChange}
             style={{ display: 'none' }}
